2809135575530fd6f230cb8949fd8b99769ca282
Jacob Champion <pchampion@pivotal.io>
2020-01-06 16:56:14 -0800

    WIP: hub: combine all configuration into a single file
    Consolidate config file creation/saving into hub.SaveConfig(). TODO talk
    about PersistedConfig
    
    Co-authored-by: Bhuvnesh Chaudhary <bchaudhary@pivotal.io>

--- cli/commanders/initialize.go
+++ cli/commanders/initialize.go
@@ -4,14 +4,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path"
 	"path/filepath"
 
-	"github.com/pkg/errors"
-
-	"github.com/greenplum-db/gp-common-go-libs/cluster"
 	"github.com/greenplum-db/gp-common-go-libs/gplog"
 
+	"github.com/greenplum-db/gpupgrade/hub"
 	"github.com/greenplum-db/gpupgrade/utils"
 )
 
@@ -33,31 +30,14 @@ func CreateStateDirAndClusterConfigs(sourceBinDir, targetBinDir string) (err err
 		return err
 	}
 
-	// Create empty clusters in source and target so that gpupgrade hub can
-	// start without having replaced them with current values.
-	// TODO: implement a slicker scheme to allow this.
-	emptyCluster := cluster.NewCluster([]cluster.SegConfig{})
-
-	source := &utils.Cluster{
-		Cluster:    emptyCluster,
-		BinDir:     path.Clean(sourceBinDir),
-		ConfigPath: filepath.Join(stateDir, utils.SOURCE_CONFIG_FILENAME),
-	}
-	err = source.Commit()
+	// Create an empty JSON configuration for the hub.
+	file, err := os.Create(filepath.Join(stateDir, hub.ConfigFileName))
 	if err != nil {
-		return errors.Wrap(err, "Unable to save empty source cluster configuration")
+		return err
 	}
+	defer file.Close()
 
-	target := &utils.Cluster{
-		Cluster:    emptyCluster,
-		BinDir:     path.Clean(targetBinDir),
-		ConfigPath: filepath.Join(stateDir, utils.TARGET_CONFIG_FILENAME),
-	}
-	err = target.Commit()
-	if err != nil {
-		return errors.Wrap(err, "Unable to save empty target cluster configuration")
-	}
-
+	fmt.Fprintf(file, "{}") // the hub will fill this in during initialization
 	return nil
 }
 

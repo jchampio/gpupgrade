195abf89c7e8908e4c7e3359b324f7c7ceac1d1d
Jacob Champion <pchampion@pivotal.io>
2020-02-27 09:13:59 -0800

    XXX

--- ci/scripts/upgrade-cluster.bash
+++ ci/scripts/upgrade-cluster.bash
@@ -93,14 +93,6 @@ CREATE TABLE customer_addresses_dim (
     customer_id integer NOT NULL,
     valid_from_timestamp timestamp without time zone DEFAULT now() NOT NULL,
     valid_to_timestamp timestamp without time zone,
-    house_number character varying(20),
-    street_name character varying(150),
-    appt_suite_no character varying(50),
-    city character varying(200),
-    state_code character varying(2),
-    zip_code character varying(5),
-    zip_plus_four character varying(10),
-    country character varying(10),
     phone_number character varying(20)
 )
 WITH (appendonly=true, compresstype=quicklz, orientation=row) DISTRIBUTED BY (customer_id);

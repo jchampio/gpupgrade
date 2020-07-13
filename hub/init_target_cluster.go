2b5d6a89554c172696910c012cd397fe30844b58
Kalen Krempely <kkrempely@pivotal.io>
2020-07-13 16:54:30 -0700

    save catalog version
    The catalog version is used when reverting tablespaces in order to
    correctly delete the tablespace directory which is based off the catalog
    version.

--- hub/init_target_cluster.go
+++ hub/init_target_cluster.go
@@ -4,13 +4,17 @@
 package hub
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"path"
 	"path/filepath"
 	"strings"
 
 	"github.com/greenplum-db/gp-common-go-libs/dbconn"
+	"github.com/greenplum-db/gp-common-go-libs/gplog"
 	"github.com/pkg/errors"
 	"golang.org/x/xerrors"
 
@@ -18,6 +22,8 @@ import (
 	"github.com/greenplum-db/gpupgrade/greenplum"
 	"github.com/greenplum-db/gpupgrade/step"
 )
+
+var pgControldataCmd = execCommand
 
 func (s *Server) GenerateInitsystemConfig() error {
 	sourceDBConn := db.NewDBConn("localhost", int(s.Source.MasterPort()), "template1")
@@ -188,4 +194,39 @@ func GetMasterSegPrefix(datadir string) (string, error) {
 		return "", fmt.Errorf("path has no segment prefix: '%s'", datadir)
 	}
 	return segPrefix, nil
+}
+
+func GetCatalogVersion(stream step.OutStreams, gphome, datadir string) (string, error) {
+	utility := filepath.Join(gphome, "bin", "pg_controldata")
+	cmd := pgControldataCmd(utility, datadir)
+
+	// Buffer stdout to parse pg_controldata
+	stdout := new(bytes.Buffer)
+	tee := io.MultiWriter(stream.Stdout(), stdout)
+
+	cmd.Stdout = tee
+	cmd.Stderr = stream.Stderr()
+
+	gplog.Debug("determining catalog version with %s", cmd.String())
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	// parse pg_control data
+	var version string
+	prefix := "Catalog version number:"
+
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, prefix) {
+			version = strings.TrimSpace(strings.TrimPrefix(line, prefix))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return "", xerrors.Errorf("scanning pg_controldata: %w", err)
+	}
+
+	return version, nil
 }
